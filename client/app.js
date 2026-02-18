6225ecbc448735989a955edef486b3bd6767a06f
Riyuo <arcx@Riyuos-MacBook-Air.local>
2026-02-18 17:29:14 +0800

    Push

--- client/app.js
+++ client/app.js
@@ -4,6 +4,15 @@
 $(document).ready(function() {
     // chain method
 
-    
+    const $display = $("#display");
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/",
+        success: function(data){
+            console.log(data)
+            $display.text(`Message: ${data.message}`)
+        }
+    })
 
 })

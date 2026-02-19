6837ac2421ddc0540d01062bc1cbab608f7f2db3
Riyuo <arcx@Riyuos-MacBook-Air.local>
2026-02-19 17:02:22 +0800

    Push

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const PORT = 8080;
 
-const app = express()
+const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({message: "Hello! This is server!"});
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`)
-})
+});
